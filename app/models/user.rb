574759b2177868a631a566a777947ce90b5ff701
root <Love.azusa@icloud.com>
2021-08-04 08:14:34 +0000

    validation and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+ #データを保存する前にメアドを小文字にする。
+before_save { email.downcase! }
+ #nameは必ず存在する。50文字以内
+validates :name, presence: true, length: { maximum: 50 }
+#メールアドレスのフォーマットを「正規表現」を使って指定
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :email, presence: true, length: { maximum: 255 },
+                  format: { with: VALID_EMAIL_REGEX },
+                  uniqueness: { case_sensitive: false }
+has_secure_password
+validates :password, presence: true, length: {maximum: 6}           
 end
+
